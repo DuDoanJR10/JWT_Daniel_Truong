4d04fb78472829cb0fa646fd8da40a62fa441f5e
DuDoanJR10 <dudoanjr10@gmail.com>
2023-09-03 16:05:10 +0700

    Setup Backend

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const cookieParsers = require('cookie-parser');
+
+const app =  express();
+app.listen(8000, () => console.log('Server is running'))
