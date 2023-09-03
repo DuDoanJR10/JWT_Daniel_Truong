4ce661db4408b348d696ba352e3a421aaf2d099b
DuDoanJR10 <dudoanjr10@gmail.com>
2023-09-03 16:19:08 +0700

    Connect to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); 
 const mongoose = require('mongoose');
 const cookieParsers = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 
-const app =  express();
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+dotenv.config();
+
+const mongoDB = process.env.MONGODB_URL;
+mongoose
+    .connect(mongoDB)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+
+    .catch((err) => {
+        console.log(err);
+    });
+
 app.listen(8000, () => console.log('Server is running'))
