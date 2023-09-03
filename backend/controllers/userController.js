dd961ef5e4955a620e6fe717e4ea7b20f68c46f1
DuDoanJR10 <dudoanjr10@gmail.com>
2023-09-03 17:42:33 +0700

    Route deleteUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,21 @@
 const User = require('../models/User');
 
 const userController = {
-    
+    // GET: /v1/users/
     getAllUsers: async (req, res) => {
         try {
             // Get all users
             const users = await User.find();
             res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // DELETE: /v1/users/:id
+    deleteUser: async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            res.status(200).json("Delete success!");
         } catch (err) {
             res.status(500).json(err);
         }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,4 +4,6 @@ const router = require("express").Router();
 
 router.get("/", userController.getAllUsers);
 
+router.delete("/:id", userController.deleteUser);
+
 module.exports = router;
