81e11f73c8d9bbfe416bb96dace0b08851bedd67
DuDoanJR10 <dudoanjr10@gmail.com>
2023-09-03 17:24:04 +0700

    Route loginUser

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
 const authController = {
+    // POST: /v1/auth/register
     registerUser: async(req, res) => {
         try {
             // Hash password
@@ -18,6 +19,27 @@ const authController = {
             // Save to database
             const user = await newUser.save();
             res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // POST: /v1/auth/login
+    loginUser: async (req, res) => {
+        try {
+            const user = await User.findOne({ username: req.body.username })
+            if (!user) {
+                res.status(404).json("Wrong username!");
+            }
+            const validPassword = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+            if (!validPassword) {
+                res.status(404).json("Wrong password!");
+            }
+            if (user && validPassword) {
+                res.status(200).json(user);
+            }
         } catch (err) {
             res.status(500).json(err);
         }
