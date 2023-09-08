96cfd6227764fbe5db0fc56a258aa7252cf294f8
DuDoanJR10 <dudoanjr10@gmail.com>
2023-09-08 22:16:56 +0700

    Add frontend

---
+++ frontend/src/Components/Login/Login.jsx
@@ -0,0 +1,24 @@
+import "./login.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    return ( 
+        <section className="login-container">
+            <div className="login-title"> Log in</div>
+            <form>
+                <label>USERNAME</label>
+                <input name="username" type="text" placeholder="Enter your username" onChange={(e) => setUsername(e.target.value)} />
+                <label>PASSWORD</label>
+                <input name="password" type="password" placeholder="Enter your password" onChange={(e) => setPassword(e.target.value)} />
+                <button type="submit">Continue</button>
+            </form>
+            <div className="login-register"> Don't have an account yet? </div>
+            <Link className="login-register-link" to="/register">Register one for free </Link>
+        </section>
+     );
+}
+ 
+export default Login;
